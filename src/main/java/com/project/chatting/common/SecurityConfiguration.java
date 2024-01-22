34143ecf47289bde5d5cfd2f78cc2c66cb11df50
hyojin <hi8087470@gmail.com>
2024-01-22 17:30:18 +0900

    JWT설정 first init

--- src/main/java/com/project/chatting/common/SecurityConfiguration.java
+++ src/main/java/com/project/chatting/common/SecurityConfiguration.java
@@ -1,10 +1,11 @@
 package com.project.chatting.common;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,15 +16,24 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.project.chatting.auth.AuthEntryPointJwt;
+import com.project.chatting.auth.JwtTokenFilter;
 
 
 @Configuration
 @EnableWebSecurity
 
 public class SecurityConfiguration{
-
 	
+	@Autowired
+	private AuthEntryPointJwt unauthorizedHandler;
+	
+	@Bean
+	public JwtTokenFilter jwtTokenFilter() {
+		return new JwtTokenFilter();
+	}
 	
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
@@ -44,13 +54,20 @@ public class SecurityConfiguration{
 	@Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-          .csrf(AbstractHttpConfigurer::disable).
+          .csrf(AbstractHttpConfigurer::disable).exceptionHandling(exceptionHandling -> exceptionHandling
+                  .authenticationEntryPoint(unauthorizedHandler)
+                ).
           sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
           .authorizeHttpRequests(authorizeRequests -> authorizeRequests
             // 모두 허용
             .requestMatchers(
-            		  "/**" 
-            ).permitAll());
+            		  "/user/auth/signin", // 로그인
+            	      "/user/auth/signup"  
+            ).permitAll()
+            // 그 외는 인증 필요
+            .anyRequest().authenticated())
+          // jwt filter 추가
+          .addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
