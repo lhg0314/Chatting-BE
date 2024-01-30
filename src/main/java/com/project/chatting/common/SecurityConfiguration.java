87b9ae6ac17fe57065af062f6e1e96344c1f08aa
hyojin <hi8087470@gmail.com>
2024-01-30 15:09:23 +0900

    localhost:3000 cors해제

--- src/main/java/com/project/chatting/common/SecurityConfiguration.java
+++ src/main/java/com/project/chatting/common/SecurityConfiguration.java
@@ -1,15 +1,15 @@
 package com.project.chatting.common;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -17,9 +17,14 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.project.chatting.auth.AuthEntryPointJwt;
 import com.project.chatting.auth.JwtTokenFilter;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @Configuration
@@ -51,9 +56,22 @@ public class SecurityConfiguration{
         return webSecurity -> webSecurity.ignoring().requestMatchers("/docs/**", "/error", "/v3/api-docs/**","/resources/**");
     }
 	
+	   @Bean
+	    public CorsConfigurationSource corsConfigurationSource() {
+	        CorsConfiguration config = new CorsConfiguration();
+
+	        config.setAllowCredentials(true);
+	        config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+	        config.setAllowedMethods(Arrays.asList("HEAD","POST","GET","DELETE","PUT"));
+	        config.setAllowedHeaders(Arrays.asList("*"));
+
+	        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	        source.registerCorsConfiguration("/**", config);
+	        return source;
+	    }
 	@Bean
     protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()))
           .csrf(AbstractHttpConfigurer::disable).exceptionHandling(exceptionHandling -> exceptionHandling
                   .authenticationEntryPoint(unauthorizedHandler)
                 ).
